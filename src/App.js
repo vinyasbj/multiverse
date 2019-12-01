c042e32dffe80993ef14f198b8a29cae192cbd7c
vinyasbj <vinyas555@gmail.com>
2019-12-01 22:40:50 +0530

    few changes with image pop up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import './App.css';
+import Image from './components/image'
+// import './App.css';
 import './assets/css/main.css';
 import axios from 'axios';
+import { parseString } from 'xml2js';
+import ImageModal from './components/ImageModal';
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      images: []
+      testing: true,
+      images: [],
+      showContactPanel: false,
+      showImageModal: false,
+      imageToShow: 0
     }
   }
+
+  toggleContactPanel() {
+    this.setState({ showContactPanel: !this.state.showContactPanel });
+  }
+
+  closeContactPanel() {
+    this.setState({ showContactPanel: false });
+  }
+
+  // clicking on an image should ensure the contact panel is closed
+  imageClicked(index) {
+    if (this.state.showContactPanel) {
+      this.toggleContactPanel();
+    }
+    else {
+      this.setState({
+        showImageModal: !this.state.showImageModal,
+        imageToShow: index
+      });
+      console.log(this.state.showImageModal);
+    }
+  }
+
+  closeImageModal() {
+    this.setState({ showImageModal: false });
+  }
+
   componentWillMount() {
     axios.get(`https://pixabay.com/api/?key=14471226-1404c6f1117ad2fdad8fc6199&q=yellow+flowers&image_type=photo&pretty=true`)
     .then((response)=>{
@@ -33,61 +67,35 @@ class App extends React.Component {
         </nav>
       </header>
     <div id="main">
-    {this.state.images.map(image => {
+    {/* {this.state.images.map((image ,index)=> {
         return <article className="thumb">
         <a href={image.largeImageURL} className="image"><img src={image.webformatURL} alt="" /></a>
         <h2>Magna feugiat lorem</h2>
         <p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
       </article>
-    })}
-    </div>
+    })} */}
+    {/* {this.state.images.map((image ,index) => {
+    <Image key={index} src={image.largeImageURL} source_url={image.webformatURL} duration={index} test={false} onClick={this.imageClicked.bind(this, index)} blur={this.state.showImageModal} />})} */}
 
-    <footer id="footer" className="panel">
-      <div className="inner split">
-        <div>
-          <section>
-            <h2>Magna feugiat sed adipiscing</h2>
-            <p>Nulla consequat, ex ut suscipit rutrum, mi dolor tincidunt erat, et scelerisque turpis ipsum eget quis orci mattis aliquet. Maecenas fringilla et ante at lorem et ipsum. Dolor nulla eu bibendum sapien. Donec non pharetra dui. Nulla consequat, ex ut suscipit rutrum, mi dolor tincidunt erat, et scelerisque turpis ipsum.</p>
-          </section>
-          <section>
-            <h2>Follow me on ...</h2>
-            <ul className="icons">
-              {/* <li><a href="" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-              <li><a href="" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-              <li><a href="" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-              <li><a href="" className="icon fa-github"><span className="label">GitHub</span></a></li>
-              <li><a href="" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-              <li><a href="" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li> */}
-            </ul>
-          </section>
-          <p className="copyright">
-            &copy; Unttled. Design: <a href="http://html5up.net">HTML5 UP</a>.
-          </p>
-        </div>
-        <div>
-          <section>
-            <h2>Get in touch</h2>
-            <form method="post" action="">
-              <div className="fields">
-                <div className="field half">
-                  <input type="text" name="name" id="name" placeholder="Name" />
-                </div>
-                <div className="field half">
-                  <input type="text" name="email" id="email" placeholder="Email" />
-                </div>
-                <div className="field">
-                  <textarea name="message" id="message" rows="4" placeholder="Message"></textarea>
-                </div>
-              </div>
-              <ul className="actions">
-                <li><input type="submit" value="Send" className="primary" /></li>
-                <li><input type="reset" value="Reset" /></li>
-              </ul>
-            </form>
-          </section>
-        </div>
-      </div>
-    </footer>
+    {
+            this.state.images.map((image, index) => (
+              <Image
+                key={index}
+                src={image.largeImageURL}
+                source_url={image.largeImageURL}
+                duration={index}
+                test={false}
+                onClick={this.imageClicked.bind(this, index)}
+                blur={this.state.showImageModal}
+              />
+            ))
+          }
+    </div>
+    <ImageModal
+          show={this.state.showImageModal}
+          imageToShow={this.state.imageToShow}
+          images={this.state.images}
+          closeCallback={this.closeImageModal.bind(this)} />
 </div>
   );
   }
---
+++ src/components/ImageModal.js
@@ -0,0 +1,281 @@
+import React, { Component } from 'react';
+import  {spring,Motion }from 'react-motion';
+// import  spring from 'react-motion';
+import Img from 'react-image';
+import Loader from 'react-loader';
+import MdChevronRight from 'react-icons/md/index';
+import MdChevronLeft from 'react-icons/md/index';
+import MdClose from 'react-icons/md/index';
+
+class ImageModal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            leftActive: false,
+            rightActive: false,
+            closeActive: false,
+            show: false,
+            imageToShow: undefined,
+            nextImageToShow: undefined,
+            images: [],
+            showCaption: false,
+            pictureShown: false
+        }
+    }
+
+    clickHandler(name, proxy, event) {
+        proxy.stopPropagation();
+        switch (name) {
+            case 'left':
+                this.setState({ showPicture: false });
+                let nextImageToShowLeft = this.state.imageToShow === 0 ?
+                    this.state.images.length - 1 :
+                    (this.state.imageToShow - 1) % this.state.images.length
+                this.setState({
+                    nextImageToShow: nextImageToShowLeft
+                });
+                this.swapImages(this.state.imageToShow, nextImageToShowLeft);
+                break;
+            case 'right':
+                this.setState({ showPicture: false });
+                let nextImageToShowRight = (this.state.imageToShow + 1) % this.state.images.length
+
+                this.setState({
+                    nextImageToShow: nextImageToShowRight
+                });
+                this.swapImages(this.state.imageToShow, nextImageToShowRight);
+                break;
+            case 'close':
+                this.close();
+                break;
+            default:
+                console.log('error');
+                break;
+        };
+
+    }
+
+    handleMouseEnter(name) {
+        switch (name) {
+            case 'left':
+                this.setState({ leftActive: true });
+                break;
+            case 'right':
+                this.setState({ rightActive: true });
+                break;
+            case 'close':
+                this.setState({ closeActive: true });
+                break;
+            default:
+                console.log('error');
+                break;
+        }
+    }
+    handleMouseLeave(name) {
+        switch (name) {
+            case 'left':
+                this.setState({ leftActive: false });
+                break;
+            case 'right':
+                this.setState({ rightActive: false });
+                break;
+            case 'close':
+                this.setState({ closeActive: false });
+                break;
+            default:
+                console.log('error');
+                break;
+        }
+    }
+
+    close() {
+        this.setState({
+            show: false,
+            showCaption: false,
+            pictureShown: false
+        });
+        this.props.closeCallback();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log(nextProps)
+        this.setState({
+            images: nextProps.images,
+            imageToShow: nextProps.imageToShow,
+            nextImageToShow: undefined,
+            show: nextProps.show,
+            pictureShown: nextProps.show
+        });
+        debugger
+    }
+
+    animationComplete(a, b, c) {
+        console.log('animation complete');
+        // debugger;
+        if ((this.state.show) && (!this.state.showCaption)) {
+            this.raf = requestAnimationFrame(() => this.setState({ showCaption: true }));
+        }
+        else if ((this.state.show) && (!this.state.pictureShown)) {
+            this.raf = requestAnimationFrame(() => this.setState({
+                imageToShow: this.state.nextImageToShow,
+                pictureShown: true
+            }));
+        }
+    }
+
+
+    swapImages(currentPictureIndex, nextPictureIndex) {
+        console.log(`swapping images: ${currentPictureIndex} => ${nextPictureIndex}`);
+        debugger;
+        this.setState({ pictureShown: false });
+        //this.setState({ imageToShow: nextPictureIndex });
+    }
+
+    render() {
+
+
+        return ( 
+            <div>
+            <Motion
+                style={{
+                    top: spring(this.state.show ? 0 : 50),
+                    left: spring(this.state.show ? 0 : 50),
+                    bottom: spring(this.state.show ? 0 : 50),
+                    right: spring(this.state.show ? 0 : 50),
+                    imageVerticalScale: spring(this.state.pictureShown ? 1 : 0),
+                    imageHorizontalScale: spring(this.state.pictureShown ? 1 : 0),
+                    captionOpacity: spring(this.state.showCaption ? 1 : 0),
+                    captionScale: spring(this.state.showCaption ? 1 : 0),
+                    leftOpacity: spring(this.state.leftActive ? 1 : .5),
+                    rightOpacity: spring(this.state.rightActive ? 1 : .5),
+                    closeOpacity: spring(this.state.closeActive ? 1 : .5),
+                }}
+                onRest={this.animationComplete.bind(this)}>
+                {({ top, left, bottom, right, imageVerticalScale, imageHorizontalScale,  
+                    captionOpacity, captionScale, leftOpacity, rightOpacity, closeOpacity }) =>
+                    <div id='imageModal'
+                        style={{
+                            zIndex: 100,
+                            position: 'fixed',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            top: top + '%',
+                            right: right + '%',
+                            bottom: bottom + '%',
+                            left: left + '%',
+                        }}
+                        onClick={this.close.bind(this)}>
+                        <div id='pictureContainer'
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                transform: `scale(${imageVerticalScale}, ${imageHorizontalScale})`,
+                                maxWidth: '75%',
+                                maxHeight: '75%',
+                                minWidth: '33%',
+                                minHeight: '33%',
+                                boxShadow: `0px 0px 5px 1px rgba(0,0, 10, 7, .75) inset`,
+                                backgroundImage: `url(${this.state.imageToShow !== undefined ?
+                                    this.state.images[this.state.imageToShow].webformatURL :
+                                    `images/fulls/01.jpg`})`,
+                                backgroundSize: 'cover',
+                            }}>
+                            <div className="closer"
+                                style={{
+                                    position: 'absolute',
+                                    left: '0px',
+                                    top: '0px',
+                                    width: '40px',
+                                    height: '40px',
+                                }}></div>
+                            <Img
+                                style={{
+                                    width: '100%',
+                                    height: '100%',
+                                    visibility: `hidden`
+                                }}
+                                src={
+                                    this.state.imageToShow !== undefined ?
+                                        this.state.images[this.state.imageToShow].webformatURL :
+                                        null
+                                }
+                                loader={<Loader />}
+                            />
+                            <div className='caption'
+                                style={{
+                                    color: 'white',
+                                    width: '100%',
+                                    position: 'absolute',
+                                    bottom: '0',
+                                    opacity: captionOpacity,
+                                    transform: `scale(${captionScale})`,
+                                    background: `linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0.64) 99%,rgba(0,0,0,0.65) 100%)`,
+                                    padding: '0px 5px 0px 5px'
+                                }}>
+                                <h2
+                                    style={{
+                                        fontFamily: "'Open Sans', sans-serif"
+                                    }}>Lorem Ipsum - Open Sans</h2>
+                                <p style={{
+                                    margin: '0em 0em .5em 0em',
+                                    fontFamily: "'Roboto', sans-serif"
+                                }}>Roboto: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..</p>
+                            </div>
+                            <div id='controls'
+                                style={{
+                                    position: 'absolute',
+                                    top: '40%',
+                                    bottom: '40%',
+                                    left: '0px',
+                                    right: '0px',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                }}
+                            >
+                                <MdChevronLeft size={64}
+                                    style={{
+                                        opacity: leftOpacity,
+                                        color: `white`
+                                    }}
+                                    onMouseEnter={this.handleMouseEnter.bind(this, 'left')}
+                                    onMouseLeave={this.handleMouseLeave.bind(this, 'left')}
+                                    onClick={this.clickHandler.bind(this, 'left')}
+                                />
+                                <MdChevronRight size={64}
+                                    style={{
+                                        opacity: rightOpacity,
+                                        color: `white`
+                                    }}
+                                    onMouseEnter={this.handleMouseEnter.bind(this, 'right')}
+                                    onMouseLeave={this.handleMouseLeave.bind(this, 'right')}
+                                    onClick={this.clickHandler.bind(this, 'right')} />
+                            </div>
+
+                            <MdClose size={48}
+                                style={{
+                                    position: 'absolute',
+                                    top: 0,
+                                    right: 0,
+                                    opacity: closeOpacity,
+                                    color: `white`
+                                }}
+                                onMouseEnter={this.handleMouseEnter.bind(this, 'close')}
+                                onMouseLeave={this.handleMouseLeave.bind(this, 'close')}
+                                onClick={this.clickHandler.bind(this, 'close')}
+                            />
+
+                        </div>
+                        <div className='nav-previous' />
+                    </div>
+                }
+            </Motion>
+            </div>
+        )
+    }
+}
+
+export default ImageModal;
+
